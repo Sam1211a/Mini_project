fdc5848cfcac2726462862c248a3c4e6a9899d5c
Sam1211a <sabbirsam65@gmail.com>
2026-04-08 18:55:27 +0600

    Error Handled

--- handlers/register.go
+++ handlers/register.go
@@ -2,12 +2,28 @@ package handlers
 
 import (
 	"fmt"
+	"html/template"
 	"mini_project/models"
 	"net/http"
 	"strings"
 )
 
+// type PageData struct {
+// 	Error    string
+
+// 	PhnError string
+
+// }
+
+var temp = template.Must(template.ParseFiles("templates/register.html"))
+
 func RegisterHandle(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "GET" {
+		temp.Execute(w, map[string]string{
+			"Error": "",
+		})
+		return
+	}
 	if r.Method == "POST" {
 		Name1 := r.FormValue("name")
 		Email := r.FormValue("email")
@@ -15,22 +31,60 @@ func RegisterHandle(w http.ResponseWriter, r *http.Request) {
 		Country := r.FormValue("country")
 		Pass := r.FormValue("pass")
 		ConfPass := r.FormValue("confpass")
-		if Pass != ConfPass {
-			http.Error(w, "Password doesn't match", 400)
-			return
-		}
 		isValidemail := strings.Contains(Email, "@")
 		isValidphn := strings.Contains(Phone, "01") && len(Phone) == 11
+		if Name1 == "" {
+			temp.Execute(w, map[string]string{
+				"ErrName": "All Fields are required !",
+				"Name":    Name1,
+				"Email":   Email,
+				"Country": Country,
+				"Phone":   Phone,
+			})
+			return
+		}
 		if isValidemail == false {
-			http.Error(w, "Invalid Email", 400)
+			temp.Execute(w, map[string]string{
+				"Error":   "invalid email",
+				"Name":    Name1,
+				"Email":   Email,
+				"Country": Country,
+				"Phone":   Phone,
+			})
 			return
 		}
+		if Country == "" {
+			temp.Execute(w, map[string]string{
+				"ErrCountry": "All Fields are required !",
+				"Name":       Name1,
+				"Email":      Email,
+				"Country":    Country,
+				"Phone":      Phone,
+			})
+			// temp.Execute(w, Error)
+			return
+		}
+
+		// temp.Execute(w, map[string]string{})
 		if isValidphn == false {
-			http.Error(w, "Invalid Phone", 400)
+			temp.Execute(w, map[string]string{
+				"PhnError": "Invalid phone",
+				"Name":     Name1,
+				"Email":    Email,
+				"Country":  Country,
+				"Phone":    Phone,
+			})
 			return
 		}
-		if Name1 == "" || Country == "" || Pass == "" {
-			http.Error(w, "All Fields are required", 400)
+
+		if Pass != ConfPass || Pass == "" {
+			temp.Execute(w, map[string]string{
+				"PassError": "Password doesn't match",
+				"Name":      Name1,
+				"Email":     Email,
+				"Country":   Country,
+				"Phone":     Phone,
+			})
 			return
 		}
 		sqlst := `insert into information (name,email,phone,country,password) values($1,$2,$3,$4,$5)`
@@ -39,13 +93,20 @@ func RegisterHandle(w http.ResponseWriter, r *http.Request) {
 			http.Error(w, err.Error(), 500)
 			return
 		}
-		fmt.Fprintln(w, "User Register succesfully")
+		http.ServeFile(w, r, "templates/register_succesfully.html")
 		return
 	}
 	http.ServeFile(w, r, "templates/register.html")
 }
 
 func LoginHandler(w http.ResponseWriter, r *http.Request) {
+	var temp = template.Must(template.ParseFiles("templates/login.html"))
+	if r.Method == "GET" {
+		temp.Execute(w, map[string]string{
+			"EmailError": "",
+		})
+		return
+	}
 	if r.Method == "POST" {
 		Emil := r.FormValue("emil")
 		Pass := r.FormValue("pass")
@@ -53,11 +114,16 @@ func LoginHandler(w http.ResponseWriter, r *http.Request) {
 		err := models.Db.QueryRow("select name,password from information where email = $1", Emil).Scan(&Uname, &checkPass)
 		// fmt.Fprintln(w, Uname, checkPass, Emil)
 		if err != nil {
-			http.Error(w, "User not Found", 400)
+			temp.Execute(w, map[string]string{
+				"EmailError": "User Email is not valid !",
+				"Email":      Emil,
+			})
 			return
 		}
 		if Pass != checkPass {
-			http.Error(w, "Password Incorrect", 400)
+			temp.Execute(w, map[string]string{
+				"PassEr": "Incorrect Password !",
+			})
 			return
 		}
 		fmt.Fprintln(w, "Congratulations ! ", Uname, " Login Succesfully.")
